457ffa31dc0ab075494d07e9f7cc6ea591b21785
Katie Ashlin <akka726@outlook.com>
2018-11-05 18:49:59 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -41,5 +41,12 @@ def sum_array(array)
 end
 
 def add_s(array)
-  [1,2].each_with_index{|word, index| }
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
+end
   
